6dfd130899c36eefb354faf12d2eab830bd2fb34
sanaeouj <sanae.ouajhain@gmail.com>
2025-04-18 17:24:56 +0100

    commit

--- Server/index.js
+++ Server/index.js
@@ -1,35 +1,39 @@
- const express = require("express");
+const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const { loginToVTiger, getContacts } = require("./vtigerAPI");
 require("dotenv").config();
-const contactRoutes = require('./routes/contacts');  
+
 const app = express();
-const PORT = process.env.PORT || 5000;
-
+const PORT = 5000;
 app.use(cors());
-app.use(express.json()); 
+app.use(bodyParser.json());
 
- app.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("📡 API backend for DataBank is running");
 });
 
- app.use('/api/contacts', contactRoutes);  
+ app.post("/api/login", async (req, res) => {
+  try {
+    const sessionName = await loginToVTiger();
+     res.json({ success: true, sessionName });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message || err });
+  }
+});
+
+
+app.get("/api/contacts", async (req, res) => {
+  try {
+     let sessionName;
+     sessionName = await loginToVTiger();
+    const contacts = await getContacts(sessionName);
+    res.json({ success: true, contacts });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`✅ Backend running at http://localhost:${PORT}`);
 });
-
- const express = require('express');
-const router = express.Router();
-const { loginToVTiger, getContacts } = require('../vtigerAPI');
-
-router.get('/', async (req, res) => {  
-  try {
-      const sessionName = await loginToVTiger();
-      const contacts = await getContacts(sessionName);
-      res.json({ success: true, contacts });
-  } catch (err) {
-      res.status(500).json({ success: false, error: err.message || err});
-  }
-});
-
-module.exports = router;
