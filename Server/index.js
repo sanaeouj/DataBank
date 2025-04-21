41cdf4784115551c4bbd1f8715c94cf354f2a2ca
sanaeouj <sanae.ouajhain@gmail.com>
2025-04-21 13:11:32 +0100

    commit

--- Server/index.js
+++ Server/index.js
@@ -1,39 +1,14 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const { loginToVTiger, getContacts } = require("./vtigerAPI");
-require("dotenv").config();
-
+ const express = require('express');
 const app = express();
-const PORT = 5000;
-app.use(cors());
-app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("📡 API backend for DataBank is running");
+ app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+ app.get('/', (req, res) => {
+  res.send('Bonjour, monde!');
 });
 
- app.post("/api/login", async (req, res) => {
-  try {
-    const sessionName = await loginToVTiger();
-     res.json({ success: true, sessionName });
-  } catch (err) {
-    res.status(500).json({ success: false, error: err.message || err });
-  }
-});
-
-
-app.get("/api/contacts", async (req, res) => {
-  try {
-     let sessionName;
-     sessionName = await loginToVTiger();
-    const contacts = await getContacts(sessionName);
-    res.json({ success: true, contacts });
-  } catch (err) {
-    res.status(500).json({ success: false, error: err.message });
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`✅ Backend running at http://localhost:${PORT}`);
+ const port = 5000;
+app.listen(port, () => {
+  console.log(`Serveur démarré sur le port ${port}`);
 });
