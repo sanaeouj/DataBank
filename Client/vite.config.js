379c7a5dbd827bbfcb2a41b7017176c7a7a59da7
sanaeouj <sanae.ouajhain@gmail.com>
2025-05-19 17:27:44 +0100

    viteconfig

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
- export default defineConfig({
+export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5175,        
-    strictPort: true  
+    port: 5175,
+    strictPort: true
+  },
+  build: {
+    outDir: 'dist'  
   }
 })
