aac5f025bfdee9dfe03e16a48993d205e51293af
sanaeouj <sanae.ouajhain@gmail.com>
2025-05-19 17:52:29 +0100

    Fix Rollup and permissions issues

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5175,
-    strictPort: true
+    strictPort: true,
   },
   build: {
-    outDir: 'dist'  
-  }
-})
+    outDir: 'dist',
+    rollupOptions: {
+       external: ['@rollup/rollup-linux-x64-gnu'],  
+    },
+  },
+});
