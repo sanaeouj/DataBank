960f06e0b779fa0bea4c6207580195998b10c66e
sanaeouj <sanae.ouajhain@gmail.com>
2025-05-20 16:12:20 +0100

    commit

--- Server/index.js
+++ Server/index.js
@@ -8,7 +8,7 @@ const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
   host: process.env.DB_HOST || 'localhost',
   database: process.env.DB_NAME || 'company_db',
-  password: process.env.DB_PASSWORD || 'Weareintelligentb2b',
+  password: process.env.DB_PASSWORD || 'Eanas900811@',
   port: process.env.DB_PORT || 5432,
 });
 
