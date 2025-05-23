101653d9f596d52a30052ce9f12d897516597693
sanaeouj <sanae.ouajhain@gmail.com>
2025-05-23 16:47:41 +0100

    commit

--- Client/src/pages/Peoples.jsx
+++ Client/src/pages/Peoples.jsx
@@ -11,9 +11,7 @@ import Sidebar from "../components/Sidebar";
 import ResultsTable from "../components/ResultsTable";
 import { useLocation } from "react-router-dom";
 import { createTheme, ThemeProvider, useTheme } from "@mui/material/styles";
-
-const API_BASE_URL = "https://databank-yndl.onrender.com";
-
+ 
 const darkTheme = createTheme({
   palette: {
     mode: "dark",
@@ -50,7 +48,7 @@ const People = () => {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          `${API_BASE_URL}/api/ressources/all`
+          "http://localhost:3000/api/ressources/all"
         );
         const result = await response.json();
         setData(result);
@@ -136,6 +134,7 @@ const People = () => {
                 width: "150px",
                 fontSize: "0.8rem",
                 backgroundColor: "#333",
+
                 "& .MuiInputBase-input": {
                   py: 0.5,
                 },
@@ -149,7 +148,7 @@ const People = () => {
                 color: "white",
                 fontSize: "0.8rem",
                 padding: "4px 8px",
-                marginBottom: "8px",
+                margin: "8px",
                 backgroundColor: "#333",
                 "&:hover": {
                   backgroundColor: "black",
@@ -166,7 +165,9 @@ const People = () => {
               Please select a filter to display the table.
             </Typography>
           )}
-        </Box>
+
+        
+            </Box>
       </Box>
     </ThemeProvider>
   );
