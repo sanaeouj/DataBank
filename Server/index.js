4391dcd43899388bb554528ce1558bd05cafe18a
sanaeouj <sanae.ouajhain@gmail.com>
2025-05-23 13:49:10 +0100

    commit

--- Server/index.js
+++ Server/index.js
@@ -8,11 +8,11 @@ const port = process.env.PORT || 3000;
 
 // Configuration de la base de données
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  user: 'data_db_uuyi_user',
+  host: 'dpg-d0o51pmmcj7s73e4t600-a',
+  database: 'data_db_uuyi',
+  password: 'Zv6No8JP8yvSpbpPXFbgHMDvZXs06nss',
+  port: 5432,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
