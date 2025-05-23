bdd0b55ba9c80e9dd3816f82a48d72f06ce2a01e
sanaeouj <sanae.ouajhain@gmail.com>
2025-05-23 15:08:35 +0100

    commit

--- Server/index.js
+++ Server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT  ;
 
 // Configuration de la base de données
 const pool = new Pool({
